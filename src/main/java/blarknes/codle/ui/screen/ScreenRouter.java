92e7560cc8adb6f5c07ed436263cb7cafe50ea59
blarknes <blarknes@gmail.com>
2025-08-06 19:59:26 -0300

    feat: add db integration to home screen

--- src/main/java/blarknes/codle/ui/screen/ScreenRouter.java
+++ src/main/java/blarknes/codle/ui/screen/ScreenRouter.java
@@ -2,15 +2,23 @@ package blarknes.codle.ui.screen;
 
 import org.springframework.stereotype.Component;
 
+import blarknes.codle.ui.player.SettingsService;
 import blarknes.codle.ui.screen.home.HomeScreen;
 import javafx.stage.Stage;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class ScreenRouter {
+
+    // TODO: I don't like this class, think about a better way to pass this settingsService down to the Screens
+
+    private final SettingsService settingsService;
 
     public void showHomeScreen(final Stage stage) {
         HomeScreen.builder()
             .stage(stage)
+            .settingsService(settingsService)
             .build()
             .show();
     }
@@ -18,6 +26,7 @@ public class ScreenRouter {
     public void showGameScreen(final Stage stage) {
         HomeScreen.builder()
             .stage(stage)
+            .settingsService(settingsService)
             .build()
             .show();
     }
--- src/main/java/blarknes/codle/ui/screen/home/HomeScreen.java
+++ src/main/java/blarknes/codle/ui/screen/home/HomeScreen.java
@@ -1,24 +1,21 @@
 package blarknes.codle.ui.screen.home;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import blarknes.codle.ui.asset.CodleIcon;
 import blarknes.codle.ui.player.SettingsService;
 import blarknes.codle.ui.screen.Screen;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import lombok.RequiredArgsConstructor;
 import lombok.val;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 public class HomeScreen extends Screen {
 
+    private final SettingsService settingsService;
+
     public Scene getScene() {
-        // TODO: update this
         val root = new StackPane();
         final CodleIcon codleIcon = new CodleIcon();
         val icon = codleIcon.asPane();
@@ -27,8 +24,8 @@ public class HomeScreen extends Screen {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction((event) -> {
-            // TODO: think about a way to use settings service here
             System.out.println("Button clicked");
+            settingsService.setStageWidth();
         });
 
         root.getChildren().addAll(todo, icon, btn);
