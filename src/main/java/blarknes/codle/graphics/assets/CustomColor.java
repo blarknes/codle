8c2d78baf54133a9383eed4c76f086ca0b91d9b7
blarknes <blarknes@gmail.com>
2025-08-10 17:27:16 -0300

    feat: add app assets

---
+++ src/main/java/blarknes/codle/graphics/assets/CustomColor.java
@@ -0,0 +1,19 @@
+package blarknes.codle.graphics.assets;
+
+import javafx.scene.paint.Color;
+import lombok.experimental.UtilityClass;
+
+/**
+ * Wrapper class to delegate the custom colors as static imports to be used
+ * across the application.
+ */
+@UtilityClass
+public class CustomColor {
+
+    public static final Color NIGHT = Color.web("#121212");
+    public static final Color ONYX = Color.web("#3a3a3c");
+    public static final Color SATINSHEENGOLD = Color.web("#b59f3b");
+    public static final Color SEAGREEN = Color.web("#538d4e");
+    public static final Color SEASALT = Color.web("#f8f8f8");
+
+}
